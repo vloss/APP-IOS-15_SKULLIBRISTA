0692558a26bd31deaec3aa61bb87386e2b705a41
vloss <viniciusvianaloss@gmail.com>
2023-02-23 02:26:43 -0300

    Criado outlets e actions.

--- Skullibrista/Skullibrista/ViewController.swift
+++ Skullibrista/Skullibrista/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var street: UIImageView!
+    @IBOutlet weak var player: UIImageView!
+    @IBOutlet weak var viGameOver: UIView!
+    @IBOutlet weak var lbTimePlayed: UILabel!
+    @IBOutlet weak var lbInstructions: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viGameOver.isHidden = true
     }
 
-
+    @IBAction func playAgain(_ sender: UIButton) {
+    }
+    
 }
 
