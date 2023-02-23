6f1fcbcaab516de8a409e33289bbfc9c130f40fb
vloss <viniciusvianaloss@gmail.com>
2023-02-23 18:13:21 -0300

    Criando animação para imagem do centro.

--- Skullibrista/Skullibrista/ViewController.swift
+++ Skullibrista/Skullibrista/ViewController.swift
@@ -18,6 +18,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viGameOver.isHidden = true
+        
+        street.frame.size.width = view.frame.size.width * 2
+        street.frame.size.height = view.frame.size.height * 2
+        street.center = view.center
+        
+        player.center = view.center
+        player.animationImages = []
+        for i in 0...7 {
+            let image = UIImage(named: "player\(i)")!
+            player.animationImages?.append(image)
+        }
+        player.animationDuration = 0.5
+        player.startAnimating()
     }
 
     @IBAction func playAgain(_ sender: UIButton) {
